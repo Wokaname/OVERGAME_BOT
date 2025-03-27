1b65828e7c720605e87260e0fdae23bd72099434
Wokaname <kabauoverson@gmail.com>
2025-03-27 21:46:45 +0100

    Update 00_0help.js

--- scripts/cmds/00_0help.js
+++ scripts/cmds/00_0help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "亗  Ar YANㅤ亗";
+const doNotDelete = "亗  ＯＶＥＲＧＡＭＥ 亗";
 /** 
 * @author NTKhang
 * @author: do not delete it
